77cb82a94ed17f44f3c85d76688b58b3e3a7dd8d
pownallk <pownallk@gmail.com>
2016-09-07 13:26:16 -0500

    ByteWriter: Working Copy

--- ByteWriter/app/src/main/java/com/karenpownall/android/aca/bytewriter/MainActivity.java
+++ ByteWriter/app/src/main/java/com/karenpownall/android/aca/bytewriter/MainActivity.java
@@ -4,8 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String fileName = "pic.gif";
 
         int[] data = {71, 73, 70, 56, 57, 97, 13, 0, 12, 0, 145,
                 0, 0, 255, 255, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 44,
@@ -23,17 +22,19 @@ public class MainActivity extends AppCompatActivity {
                 8, 160, 56, 137, 96, 72, 77, 33, 130, 86, 37, 219, 182,
                 230, 137, 89, 82, 181, 50, 220, 103, 20, 0, 59};
 
+        File outputFile = new File("/sdcard/pic.gif");
+        FileOutputStream fos;
+
         try {
-            File path = new File(getFilesDir(), "byteWriter" );
-            File myPath = new File(path, "pic.gif");
+            fos = new FileOutputStream(outputFile);
 
-            if (!myPath.exists()) {
-                OutputStreamWriter osw = new OutputStreamWriter(openFileOutput(myPath.getAbsolutePath().
-                MODE_))
+            for (int i = 0; i < data.length; i++) {
+                fos.write(data[i]);
             }
-
+            fos.close();
         } catch (IOException e) {
-            System.out.println("Error - " + e.toString());
+            System.out.println("Error - " + e.getMessage());
         }
+
     }
 }
