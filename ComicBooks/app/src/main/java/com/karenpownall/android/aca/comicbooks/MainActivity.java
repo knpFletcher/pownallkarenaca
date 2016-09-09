15a3ca25911930317ae743271d6a05674dae6e09
pownallk <pownallk@gmail.com>
2016-09-09 12:43:46 -0500

    Comic Books: Lab assignment, correct and working

--- ComicBooks/app/src/main/java/com/karenpownall/android/aca/comicbooks/MainActivity.java
+++ ComicBooks/app/src/main/java/com/karenpownall/android/aca/comicbooks/MainActivity.java
@@ -5,18 +5,35 @@ package com.karenpownall.android.aca.comicbooks;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mComixButton;
+    private TextView mOutputText;
+    private EditText mBasePrice;
+    private EditText mConditionText;
+    private EditText mIssueText;
+    private EditText mTitleText;
+    public HashMap quality = new HashMap();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //set up hash map
-        HashMap quality = new HashMap(); //create
+        mOutputText = (TextView) findViewById(R.id.outputText);
+        mBasePrice = (EditText) findViewById(R.id.basePrice);
+        mConditionText = (EditText) findViewById(R.id.conditionText);
+        mIssueText = (EditText) findViewById(R.id.issueText);
+        mTitleText = (EditText) findViewById(R.id.titleText);
+        mComixButton = (Button) findViewById(R.id.comixButton);
+
 
         float price1 = 3.00F; //add values
         quality.put("mint", price1); //key value pair
@@ -32,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         quality.put("poor", price6);
 
         //set up a collection, create array based off Comic class
-        Comic[] comix = new Comic[3];
+        final Comic[] comix = new Comic[4];
         //add comics to collection
         comix[0] = new Comic("Amazing Spider-Man", "1A", "very fine", 12_000.00f);
         //passing in title, issue number, condition, base price
@@ -44,15 +61,37 @@ public class MainActivity extends AppCompatActivity {
         comix[2] = new Comic("Cerebus", "1A", "good", 190.00f);
         comix[2].setPrice((Float) quality.get(comix[2].condition));
 
-        for (int i = 0; i < comix.length; i++) {
-            System.out.println("Title: " + comix[i].title);
-            System.out.println("Issue: " + comix[i].issueNumber);
-            System.out.println("Condition: " + comix[i].condition);
-            System.out.println("Price: $" + comix[i].price + "\n");
-        }
+        mComixButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+            String title;
+            String issue;
+            String condition;
+            float basePrice;
 
+            //Comic userComic = new Comic;
+
+            title = mTitleText.getText().toString();
+            issue = mIssueText.getText().toString();
+            condition = mConditionText.getText().toString();
+            basePrice = Float.parseFloat(mBasePrice.getText().toString());
+
+            comix[3] = new Comic(title, issue, condition, basePrice);
+            comix[3].setPrice((Float) quality.get(comix[3].condition));
+
+            for (int i = 0; i < comix.length; i++) {
+                System.out.println("Title: " + comix[i].title);
+                System.out.println("Issue: " + comix[i].issueNumber);
+                System.out.println("Condition: " + comix[i].condition);
+                System.out.println("Price: $" + comix[i].price + "\n");
+                mOutputText.setText("Price: $" + comix[i].price);
+            }
+
+            }
+        });
     }
-    class Comic {
+
+    public class Comic {
         String title;
         String issueNumber;
         String condition;
