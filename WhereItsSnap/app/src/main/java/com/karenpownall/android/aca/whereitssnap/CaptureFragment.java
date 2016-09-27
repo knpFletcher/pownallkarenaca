47d6dbb4b38c094ed756a3785bac89f54b443416
pownallk <pownallk@gmail.com>
2016-09-27 10:48:26 -0500

    still not being ignored?

--- WhereItsSnap/app/src/main/java/com/karenpownall/android/aca/whereitssnap/CaptureFragment.java
+++ WhereItsSnap/app/src/main/java/com/karenpownall/android/aca/whereitssnap/CaptureFragment.java
@@ -1,26 +1,119 @@
 package com.karenpownall.android.aca.whereitssnap;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CaptureFragment extends Fragment {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    private static final int CAMERA_REQUEST = 123;
+    private ImageView mImageView;
 
-    @Nullable
+    //the filepath for the photo
+    String mCurrentPhotoPath;
+
+    //where the captured image is stored
+    private Uri mImageUri = Uri.EMPTY;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         //inflate the layout file then get all necessary references
         View view = inflater.inflate(R.layout.fragment_capture, container, false);
 
+        mImageView = (ImageView)view.findViewById(R.id.imageView);
+        Button btnCapture = (Button)view.findViewById(R.id.btnCapture);
+        Button btnSave = (Button)view.findViewById(R.id.btnSave);
+
+        final EditText mEditTextTitle = (EditText)view.findViewById(R.id.editTextTitle);
+        final EditText mEditTextTag1 = (EditText)view.findViewById(R.id.editTextTag1);
+        final EditText mEditTextTag2 = (EditText)view.findViewById(R.id.editTextTag2);
+        final EditText mEditTextTag3 = (EditText)view.findViewById(R.id.editTextTag3);
+
+        //listen for clicks on the caputre button
+        btnCapture.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException ex) {
+                    //error occurred while creating the file
+                    Log.e("Error", "error creating file");
+                }
+
+                //continue only if the file was successfully created
+                if (photoFile != null){
+                    mImageUri = Uri.fromFile(photoFile);
+                    cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                }
+            }
+        });
         return view;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    private File createImageFile() throws IOException{
+        //create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_hHHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName, //file name
+                ".jpg", //extension
+                storageDir //folder
+        );
+
+        //save for use with Action_View intent
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == MainActivity.RESULT_OK){
+            try{
+                mImageView.setImageURI(Uri.parse(mImageUri.toString()));
+            } catch (Exception e) {
+                Log.e ("Error", "Uri not set");
+            }
+        } else {
+            mImageUri = Uri.EMPTY;
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        //make sure we don't run out of memory
+        BitmapDrawable bd = (BitmapDrawable) mImageView.getDrawable();
+        bd.getBitmap().recycle();
+        mImageView.setImageBitmap(null);
     }
 }
