1c2d8eef06740e17d35cc2d91e24abebae12c582
pownallk <pownallk@gmail.com>
2016-10-11 12:38:23 -0500

    RepoSearch: Initial Commit

---
+++ RepoSearch_starter/app/src/main/java/com/raywenderlich/reposearch/DownloadCompleteListener.java
@@ -0,0 +1,7 @@
+package com.raywenderlich.reposearch;
+
+import java.util.ArrayList;
+
+public interface DownloadCompleteListener {
+    void downloadComplete(ArrayList<Repository> repositories);
+}
